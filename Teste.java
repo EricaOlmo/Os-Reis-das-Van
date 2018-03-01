7ef78181fde6e527c947667538ede7a632908f14
MaiconPadilha <maiconantonio29@gmail.com>
2018-03-01 20:22:29 -0300

    Commitei comendo

--- Teste.java
+++ Teste.java
@@ -8,7 +8,7 @@ public class Teste {
 =======
 		system.out.printlm("Equipe Rei da Van")
 =======
-		system.out.printlm("Hello Git")
+		system.out.printlm("Kill")
 >>>>>>> 867af692cb6ec33eb9ea7af3a7eade9185662057
 >>>>>>> dd741b8ffdb6cc925ee55807c0d99d20b656a1ff
 	}
